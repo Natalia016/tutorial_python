03d40b4c7a289b5b6ad8d46d6ccdaa5976e2de4b
unknown <nataliabermudez016@gmail.com>
2023-03-08 15:17:55 -0500

    Tipo de datos primitivos

---
+++ repasoPython/main.py
@@ -0,0 +1,25 @@
+#TIPOS DE DATOS PRIMITIVOS
+#NUMEROS ENTEROS
+
+numero1:int =57
+print(numero1)
+
+numero2 = 24
+print(numero2)
+
+#NUMEROS REALES
+numero3:float = 4.9
+print(numero3)
+
+numero4 =5.0
+print(numero4)
+
+#BOOLEANOS
+
+esColombiano: bool = True
+esArgentino = False
+print(esColombiano)
+
+#CARACTER Y CADENA DE CARACTERES
+mensaje = "cadena con una comilla simple ',una comilla doble \" y una diagonal invertida\\"
+print (mensaje)
