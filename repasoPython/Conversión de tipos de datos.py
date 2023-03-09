7433cf01d696ee613567824a9da913ee1e9a64e6
unknown <nataliabermudez016@gmail.com>
2023-03-08 22:42:46 -0500

    se agrega  conversión de tipo de datos

---
+++ repasoPython/Conversión de tipos de datos.py
@@ -0,0 +1,48 @@
+#Conversión de tipo de datos
+
+#TIPO DE DATOS NUMERICOS
+
+""""convierte un numero flotante en entero"""
+
+print("CONVERSIÓN TIPO DE DATOS NUMERICOS ")
+print("INT ")
+print(int(48.3))
+
+print("FLOAT ")
+"""convierte un numero entero en  flotante"""
+print(float(32))
+
+print("Conversión de números mediante división ")
+a = 10 / 2
+print(a)
+
+print("CONVERSIÓN DE CADENAS ")
+
+print("SRT() ")
+""""convierte los números en cadenas usando el método str(). 
+"""
+print(str(12))
+
+print("CONVERSIÓN EN TUPLAS ")
+"""
+
+"""
+print("TUPLE ")
+""""convierte una lista en tupla. 
+"""
+print(tuple(['margaritas', 'rosas', 'azucena']))
+
+print("CONVIERTE UNA TUPLA EN CADENA ")
+print(tuple('margaritas'))
+
+print("CONVIERTE  TUPLAS EN LISTAS ")
+""""Los corchetes del resultado indican que una lista 
+se devolvió a partir del valor original de la tupla """
+
+print(list(('margaritas', 'rosas', 'azucena')))
+
+
+print("CONVIERTE UNA CADENA EN LISTA ")
+"""se convierte en una lista
+"""
+print(list( 'rosas'))
